f30e688162bfe12df56afe4a0d9c217e6d54f127
Aaron Schmitz <me@aaronschmitz.com>
2012-11-28 13:39:50 -0600

    Fill in editor code. Many things are FUBAR!

--- editor4/editor.c
+++ editor4/editor.c
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 #include <curses.h>
 #include <string.h>
+#include <unistd.h>
 #include "textbuff.h"
 
 #define CTRL(c) ((c) & 037)
+#define TEMPCHAR '~'
 
 #define min(X, Y)                \
      ({ typeof (X) x_ = (X);          \
@@ -33,10 +35,15 @@ struct message_t{
     struct message_t *next;
 } typedef message;
 
+message* first;
+message* last;
 
 // mutex to control access to the text buffer
 pthread_mutex_t text_ = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t queue = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t back = PTHREAD_MUTEX_INITIALIZER;
 
+char* backup;
 
 // The current position of the cursor in the screen
 int row;
@@ -52,7 +59,14 @@ int view_max;
  * and returns it.
  */
 message* pop(){
-
+	if (first != NULL)
+	{
+		pthread_mutex_lock(&queue);
+		message* ret = first;
+		first = first->next;
+		pthread_mutex_unlock(&queue);
+		return ret;
+	}
 	return NULL;
 }
 
@@ -61,9 +75,16 @@ message* pop(){
  * Inserts a message at the back of the message queue
  */
 void push(message* m_){
-
-	
-
+	pthread_mutex_lock(&queue);	
+	if (first == NULL)
+	{
+		first = last = m_;
+	}
+	else
+	{
+		last->next = m_;
+	}
+	pthread_mutex_unlock(&queue);
 }
 
 
@@ -122,7 +143,12 @@ void input_mode(){
         }
 
         //Add code here to insert c into textbuff at (row, col) using the message queue interface.
-
+		message msg;
+		msg.data = c;
+		msg.row = row;
+		msg.col = col;
+		msg.command = EDIT;
+		push(&msg);
 
         // ------------------------------
         if(col<COLS-1){
@@ -152,6 +178,7 @@ void loop(){
         refresh();
         c = getch();
 
+		message msg;
         switch(c){
             case 'h':
             case KEY_LEFT:
@@ -195,7 +222,10 @@ void loop(){
                 flash();
 
                 // Add code here to delete character (row, col) from textbuf
-
+				msg.row = row;
+				msg.col = col;
+				msg.command = DEL;
+				push(&msg);
                 // ------------------------------
 
                 redraw(view_min,view_max,row,col,0);
@@ -204,8 +234,8 @@ void loop(){
                 flash();
 
                 // Add code here to save the textbuf file
-
-
+				msg.command = SAVE;
+				push(&msg);
                 // ------------------------------
 
                 break;
@@ -213,7 +243,8 @@ void loop(){
                 endwin();
 
                 // Add code here to quit the program
-
+				msg.command = QUIT;
+				push(&msg);
                 // ------------------------------
             default:
                 flash();
@@ -253,42 +284,139 @@ void *start_UI(void *threadid){
 void *autosave(void *threadid){
 
     // This function loops until told otherwise from the router thread. Each loop:
-
+	while (TRUE)
+	{
+		pthread_mutex_lock(&back);
         // Open the temporary file
+		FILE* fout;
+		if (((fout = fopen(backup, "w")) == 0)) {
+			perror("Cannot save file!");
+			continue;
+		}
 
         // Read lines from the text buffer and save them to the temporary file
+		pthread_mutex_lock(&text_);
+		int i, lines = getLineLength();
+		for (i = 0; i < lines; i++)
+		{
+			char* line;
+			if (getLine(i, &line) == 0)
+			{
+				fprintf(fout, "%s", line);
+			}
+		}
+		pthread_mutex_unlock(&text_);
 
         // Close the temporary file and sleep for 5 sec.
+		fclose(fout);
+		pthread_mutex_unlock(&back);
+		sleep(5);
+	}
         
-     return NULL;
+    return NULL;
 
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 	printf("CSCI 4601 Lab 4 Editor\n");
     row = 0;
     col = 0;
+	
+	pthread_t ui_thread, as_thread;
 
     // get text file from argv
+	char* filename;
+	if (argc == 2) {
+		filename = argv[1];
+	} else {
+		printf("Incorrect number of arguments.\n");
+		exit(1);
+	}
+	
+	int len = strlen(filename);
+	backup = malloc(len + 2);
+	if (backup == NULL)
+	{
+		perror("Could not allocate memory!");
+		return 0;
+	}
+	strcpy(backup, filename);
+	backup[len] = TEMPCHAR;
+	backup[len + 1] = '\0';
 
     // set up necessary data structures for message passing queue and initialize textbuff
+	last = first = NULL;
+	init_textbuff(filename);
 
     // spawn UI thread
+	int ui_id = pthread_create(&ui_thread, NULL, (void *) &start_UI, NULL);
 
     // spawn auto-save thread
+	int as_id = pthread_create(&as_thread, NULL, (void *) &autosave, NULL);
 
     // Main loop until told otherwise
-
+	bool quit = FALSE;
+	while (!quit)
+	{
         // Recieve messages from the message queue
+		message* msg = pop();
+		
+		if (msg == NULL) continue;
 
-        // If EDIT then place the edits into the text buffer
+		switch (msg->command)
+		{
+			// If EDIT then place the edits into the text buffer
+			case EDIT:
+				pthread_mutex_lock(&text_);
+				insert(msg->row, msg->col, msg->data);
+				pthread_mutex_unlock(&text_);
+				break;
 
-        // If SAVE then save the file additionally delete the temporary save
+			// If SAVE then save the file additionally delete the temporary save
+			case SAVE:
+				pthread_mutex_lock(&back);
+				FILE* fout;
+				if (((fout = fopen(filename, "w")) == 0)) {
+					perror("Cannot save file!");
+					continue;
+				}
 
-        // If QUIT then quit the program and tell the appropriate threads to stop
+				int i, lines = getLineLength();
+				for (i = 0; i < lines; i++)
+				{
+					char* line;
+					if (!getLine(i, &line))
+					{
+						fprintf(fout, "%s", line);
+					}
+				}
+				unlink(backup);
+				
+				pthread_mutex_unlock(&back);
+				break;
 
-        // If DEL then delete the specified character from the text buffer
+			// If QUIT then quit the program and tell the appropriate threads to stop
+			case QUIT:
+				quit = TRUE;
+				break;
+
+			// If DEL then delete the specified character from the text buffer
+			case DEL:
+				pthread_mutex_lock(&text_);
+				deleteCharacter(msg->row, msg->col);
+				pthread_mutex_unlock(&text_);
+				break;
+		}	
+	}
 
     // Clean up data structures
+	pthread_mutex_lock(&back);
+	pthread_cancel(as_id);
+	pthread_mutex_unlock(&back);
+
+	pthread_cancel(ui_id);
+	deleteBuffer();
+	free(backup);
+	while(pop());
     return 0;
 }
